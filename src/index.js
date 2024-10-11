750d684344c5e1e5ede70f66d0fbba97914e7e3e
Robiul Hasan <muhammadrobiul502@gmail.com>
2024-10-11 17:53:48 +0600

    latest

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/', require('./routes/root'))
+app.use('/', require('./routes/root'))
 app.use('/api/auth', require('./routes/authRoute'))
 app.use('/api/user', require('./routes/userRoute'))
 
